a44dd2e999dfb6550d8ff3fa8c56252bc04e8eea
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2024-08-19 12:09:01 +0700

    add class (listHeroAdapter) & library (Glide)

---
+++ app/src/main/java/com/practice/apphero/ListHeroAdapter.kt
@@ -0,0 +1,33 @@
+package com.practice.apphero
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class ListHeroAdapter(private val listHero: ArrayList<Hero>): RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+        val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        val tvDescription: TextView = itemView.findViewById(R.id.tv_item_descripstion)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = listHero.size
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val (name, description, photo) = listHero[position]
+        Glide.with(holder.itemView.context)
+            .load(photo)
+            .into(holder.imgPhoto)
+    }
+
+}
